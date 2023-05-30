6acb968ee693009d319b14281faf80f29a99f5e3
Michael du Plessis <mdp0101@gmail.com>
2023-05-30 19:58:14 +0200

    implemented method generation

--- generic_builder/src/lib.rs
+++ generic_builder/src/lib.rs
@@ -37,8 +37,25 @@ fn create_fields(name_type: NameTypeIterator) -> proc_macro2::TokenStream {
     }
 }
 
-fn create_funcs() -> proc_macro2::TokenStream {
-    todo!()
+fn create_funcs(name_type: NameTypeIterator) -> proc_macro2::TokenStream {
+    let funcs = name_type.map(|(name, ty)| {
+        let ty = if let Some(ty) = helper::is(ty, "Option") {
+            ty
+        } else {
+            ty
+        };
+
+        quote! {
+            pub fn #name(&mut self, #name: impl Into<#ty>) -> &mut Self {
+                self.#name = Some(#name.into());
+                self
+            }
+        }
+    });
+
+    quote! {
+        #(#funcs)*
+    }
 }
 
 fn create_builder(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
@@ -55,10 +72,16 @@ fn create_builder(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
 
     // getting builder structs fields
     let fields = create_fields(helper::name_type_iter(named));
+    // getting builder structs methods
+    let funcs = create_funcs(helper::name_type_iter(named));
 
     quote! {
         struct #builder_name {
             #fields
+        }
+
+        impl #builder_name {
+            #funcs
         }
     }
 }
